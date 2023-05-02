8744aa48fdf4895a3afdf5a21b0f19a088578c11
blingalinga <ling10@uw.edu>
2023-05-02 12:45:08 -0700

    first commit

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -23,6 +23,40 @@ for i, arg in enumerate(sys.argv):
     # means the Pokemon is strong against that type, and less than 1 means
     # the Pokemon is weak against that type
 
+    conn = sqlite3.connect('pokemon.sqlite')
+    sql = f"""SELECT *
+            FROM imported_pokemon_data
+            WHERE pokedex_number = {arg}"""
+
+    # Execute the SQL query and get the results
+    cursor = conn.execute(sql)
+    row = cursor.fetchone()
+
+    name = row[29]
+    type1 = row[35]
+    type2 = row[36]
+    against = [float(row[i]) for i in range(4,22)]
+
+    # Analyze the strengths and weaknesses of the Pokemon
+    strengths = []
+    weaknesses = []
+
+    for j, factor in enumerate(against):
+        if factor > 1:
+            strengths.append(types[j])
+        elif factor < 1:
+            weaknesses.append(types[j])
+
+    # Print the results
+    print(f"Analyzing {arg}")
+    print(f"{name} ({type1} {type2}) is strong against {', '.join(strengths)} but weak against {', '.join(weaknesses)}")
+
+    # Add the Pokemon to the team list
+    team.append(name)
+
+    # Close the database connection
+    conn.close()
+
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
     teamName = input("Enter the team name: ")
@@ -31,4 +65,3 @@ if answer.upper() == "Y" or answer.upper() == "YES":
     print("Saving " + teamName + " ...")
 else:
     print("Bye for now!")
-
